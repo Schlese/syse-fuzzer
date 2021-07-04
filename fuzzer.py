515c25190f3d349c1a1b06ce92fe4ad4e0275633
Patrick <is201838@fhstp.ac.at>
2021-07-04 22:06:19 +0200

    remove function fuzz_username

--- fuzzer.py
+++ fuzzer.py
@@ -5,51 +5,7 @@ import socket
 import threading
 import time
 
-# https://github.com/samyoyo/SFTPfuzzer/blob/master/SFTPfuzzer.py
-# https://blog.modpr0.be/2010/09/20/very-simple-ftp-fuzzer/
-# https://stackoverflow.com/questions/50117522/how-to-connect-to-an-ftp-server-with-sockets
-# https://stackoverflow.com/questions/30965512/my-simple-client-crash-everytime-it-tries-to-connect-to-my-python-socket-server
 from helper import *
-
-
-def fuzzUsername(host, port):
-    try:
-        print("Create socket and connect to server")
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect = s.connect((host, port))
-        time.sleep(0.5)
-        output = s.recv(1024)
-        print("Recieved: " + (output.decode('utf-8')))
-        time.sleep(0.5)
-
-        print("Send 'USER' command")
-        s.send(("USER\r\n").encode('utf-8'))
-        output = s.recv(1024)
-        print("Recieved: " + (output.decode('utf-8')))
-        time.sleep(0.5)
-
-        print("Send 'PASS' command")
-        s.send(("PASS network\r\n").encode('utf-8'))
-        output = s.recv(1024)
-        print("Recieved: " + (output.decode('utf-8')))
-        time.sleep(0.5)
-
-        print("Send 'PWD' command")
-        s.send(("PWD\r\n").encode('utf-8'))
-        output = s.recv(1024)
-        print("Recieved: " + (output.decode('utf-8')))
-        time.sleep(0.5)
-
-        print("Send 'QUIT' command")
-        s.send(("QUIT\r\n").encode('utf-8'))
-        output = s.recv(1024)
-        print("Recieved: " + (output.decode('utf-8')))
-        time.sleep(0.5)
-
-        print("Close connection")
-        s.close()
-    except:
-        print("Error: Connection refused or server crashed")
 
 
 def fuzzFileAccess(host, port):
@@ -185,8 +141,6 @@ def main():
     )
     args = parser.parse_args()
 
-    if args.fuzz_user:
-        fuzzUsername(args.host, args.port)
     if args.fuzz_file_access:
         fuzzFileAccess(args.host, args.port)
     if args.commandsNoArgs:
